5b7a0477d7ba6523705d51cf7d36acdd4e6cea69
Muhammad Rizal Nurfirdaus <M.Rizal.Nurfirdaus@outlook.com>
2025-12-16 19:53:18 +0700

    chore: Initialize Gradle wrapper and scripts for modul8_navigation_drawer
    - Added gradle-wrapper.properties to configure Gradle distribution.
    - Created gradlew script for POSIX-compliant systems.
    - Created gradlew.bat script for Windows systems.
    - Set up settings.gradle.kts for project configuration and dependency management.

--- Modul8_appbar/app/src/main/java/com/pab/modul8_appbar/MainActivity.kt
+++ Modul8_appbar/app/src/main/java/com/pab/modul8_appbar/MainActivity.kt
@@ -1,20 +1,36 @@
 package com.pab.modul8_appbar
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.appbar.MaterialToolbar
+import com.pab.modul8_appbar.MenuActivity
+import com.pab.modul8_appbar.MenuFragment
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val topAppBar = findViewById<MaterialToolbar>(R.id.topAppBar)
+        topAppBar.setOnMenuItemClickListener { menuItem: MenuItem ->
+            when (menuItem.itemId) {
+                R.id.menu1 -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container, MenuFragment())
+                        .addToBackStack(null)
+                        .commit()
+                    true
+                }
+                R.id.menu2 -> {
+                    val intent = Intent(this, MenuActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+                else -> false
+            }
         }
     }
 }
